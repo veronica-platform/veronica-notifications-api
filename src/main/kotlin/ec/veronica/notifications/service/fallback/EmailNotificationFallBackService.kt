9df8ef7b7d58bfc822b317e6bee64c778859a36f
rolandopalermo <support@rolandopalermo.com>
2022-06-27 20:26:56 -0500

    Add email fallback service

--- src/main/kotlin/ec/veronica/notifications/service/fallback/EmailNotificationFallBackService.kt
+++ src/main/kotlin/ec/veronica/notifications/service/fallback/EmailNotificationFallBackService.kt
@@ -3,19 +3,22 @@ package ec.veronica.notifications.service.fallback
 import ec.veronica.notifications.dto.EmailNotificationDto
 import java.util.Base64
 import javax.mail.util.ByteArrayDataSource
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 
 @Service
 class EmailNotificationFallBackService(
-    private var javaMailSender: JavaMailSender
+    private var javaMailSender: JavaMailSender,
+    @Value("\${veronica.email.no-reply}")
+    private val noReplyEmailAddress: String,
 ) {
 
     fun sendMessage(request: EmailNotificationDto) {
         val msg = javaMailSender.createMimeMessage()
         val helper = MimeMessageHelper(msg, true)
-        helper.setFrom(request.name, request.name)
+        helper.setFrom(noReplyEmailAddress, request.name)
         helper.setTo(request.tos.toTypedArray())
         helper.setSubject(request.subject)
         helper.setText(request.content, true)
